9da3804481637f64f82e0cde2a1f4374118cdb0b
sekikawa kenyu <k.sekikawa@unison-planet.com>
2022-05-29 21:32:24 +0900

    imageResize2000,1500,1000,500

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,8 @@ const rename = require('gulp-rename'); //リネーム
 const imagemin = require('gulp-imagemin'); //画像圧縮
 const pngquant = require('imagemin-pngquant'); //png画像の圧縮 
 const changed = require('gulp-changed'); //画像の差分を取得
+const filelog = require('gulp-filelog'); //処理されたファイル名をログに流す
+const imageResize = require('gulp-image-resize'); //画像をリサイズする
 
 /// abisu(stylus)コンパイル //////////////////////////////////////////
 const srcAbisu = {
@@ -279,37 +281,175 @@ const browserSyncFunc = () => {
   })
 }
 
-/// 画像圧縮 //////////////////////////////////////////
+
 const srcImage = {
-  srcDir: 'html/compression/*.{jpg,jpeg,png,gif,svg}',
+  comDir: 'html/compression/*.{jpg,jpeg,png,gif,svg}',
+  resize2000Dir: 'html/resize/to_2000/*.{jpg,jpeg,png,gif,svg}',
+  resize1500Dir: 'html/resize/to_1500/*.{jpg,jpeg,png,gif,svg}',
+  resize1000Dir: 'html/resize/to_1000/*.{jpg,jpeg,png,gif,svg}',
+  resize500Dir: 'html/resize/to_500/*.{jpg,jpeg,png,gif,svg}',
   dstDir: 'html/images'
 }
+const mozjpegOptions = {
+  quality: 80
+}
+const svgoOptions = {
+  plugins: [{
+    removeViewBox: true
+  }, {
+    cleanupIDs: false
+  }]
+}
+const gifsicleOptions = {
+  interlaced: true
+}
+const pngquantOptions = {
+  quality: [0.65, 0.8],
+  speed: 1
+}
 
+/// 画像圧縮のみ //////////////////////////////////////////
 function imageMinCom() {
-  return src(srcImage.srcDir)
+  return src(srcImage.comDir)
     .pipe(changed(srcImage.dstDir))
     .pipe(
       imagemin([
         imagemin.mozjpeg({
-          quality: 80
+          mozjpegOptions
         }),
         imagemin.svgo({
-          plugins: [{
-            removeViewBox: true
-          }, {
-            cleanupIDs: false
-          }],
+          svgoOptions
         }),
         imagemin.gifsicle({
-          interlaced: true,
+          gifsicleOptions
         }),
         pngquant({
-          quality: [0.65, 0.8],
-          speed: 1,
+          pngquantOptions
         }),
       ])
     )
     .pipe(dest(srcImage.dstDir))
+}
+
+/// 画像リサイズ＋圧縮 //////////////////////////////////////////
+function imageResize2000() {
+  return src(srcImage.resize2000Dir)
+    .pipe(changed(srcImage.dstDir))
+    .pipe(
+      imageResize({
+        width: 2000,
+        crop: false,
+        upscale: false
+      })
+    )
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({
+          mozjpegOptions
+        }),
+        imagemin.svgo({
+          svgoOptions
+        }),
+        imagemin.gifsicle({
+          gifsicleOptions
+        }),
+        pngquant({
+          pngquantOptions
+        }),
+      ])
+    )
+    .pipe(dest(srcImage.dstDir))
+    .pipe(filelog())
+}
+
+function imageResize1500() {
+  return src(srcImage.resize1500Dir)
+    .pipe(changed(srcImage.dstDir))
+    .pipe(
+      imageResize({
+        width: 1500,
+        crop: false,
+        upscale: false
+      })
+    )
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({
+          mozjpegOptions
+        }),
+        imagemin.svgo({
+          svgoOptions
+        }),
+        imagemin.gifsicle({
+          gifsicleOptions
+        }),
+        pngquant({
+          pngquantOptions
+        }),
+      ])
+    )
+    .pipe(dest(srcImage.dstDir))
+    .pipe(filelog())
+}
+
+function imageResize1000() {
+  return src(srcImage.resize1000Dir)
+    .pipe(changed(srcImage.dstDir))
+    .pipe(
+      imageResize({
+        width: 1000,
+        crop: false,
+        upscale: false
+      })
+    )
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({
+          mozjpegOptions
+        }),
+        imagemin.svgo({
+          svgoOptions
+        }),
+        imagemin.gifsicle({
+          gifsicleOptions
+        }),
+        pngquant({
+          pngquantOptions
+        }),
+      ])
+    )
+    .pipe(dest(srcImage.dstDir))
+    .pipe(filelog())
+}
+
+function imageResize500() {
+  return src(srcImage.resize500Dir)
+    .pipe(changed(srcImage.dstDir))
+    .pipe(
+      imageResize({
+        width: 500,
+        crop: false,
+        upscale: false
+      })
+    )
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({
+          mozjpegOptions
+        }),
+        imagemin.svgo({
+          svgoOptions
+        }),
+        imagemin.gifsicle({
+          gifsicleOptions
+        }),
+        pngquant({
+          pngquantOptions
+        }),
+      ])
+    )
+    .pipe(dest(srcImage.dstDir))
+    .pipe(filelog())
 }
 
 
@@ -323,9 +463,13 @@ function watchFile() {
   watch(srcComponents.srcDir, series(cacheComponents, Components))
   watch(srcContents.srcDir, series(cacheContents, Contents))
   watch(srcHTML.srcDir, HTML)
-  watch(srcImage.srcDir, imageMinCom)
+  watch(srcImage.comDir, imageMinCom)
+  watch(srcImage.resize2000Dir, imageResize2000)
+  watch(srcImage.resize1500Dir, imageResize1500)
+  watch(srcImage.resize1000Dir, imageResize1000)
+  watch(srcImage.resize500Dir, imageResize500)
 }
 
 
 /// Gulpコマンドで動かすもの ////////////////////////////////////////////
-exports.default = parallel(watchFile, browserSyncFunc); //Stylusリアルタイム更新
+exports.default = parallel(watchFile, browserSyncFunc); //リアルタイム更新
